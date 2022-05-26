972db02d047c33d3a73af7ee93a2a0bdc3f6a2a2
Vladislav <vladfyklev@gmail.com>
2022-05-26 19:45:13 +0300

    добавил форматер претиер и настроил

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 function App() {
-  return (
-    <>
-      <Header />
-      <AppList />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <AppList />
+        </>
+    )
 }
 function Header() {
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    )
 }
 function AppList() {
-  return (
-    <>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </>
-  );
+    return (
+        <>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </>
+    )
 }
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<App />);
+root.render(<App />)
