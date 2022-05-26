33bcf7b5a3fa41cf061ddee6a489ac42c8dfce00
Vladislav <vladfyklev@gmail.com>
2022-05-26 20:01:42 +0300

    експорт компонента

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { App } from './container/App/App'
 
-function App() {
-    return (
-        <>
-            <Header />
-            <AppList />
-        </>
-    )
-}
-function Header() {
-    return (
-        <>
-            <h1>Hello world</h1>
-        </>
-    )
-}
-function AppList() {
-    return (
-        <>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </>
-    )
-}
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
 root.render(<App />)
