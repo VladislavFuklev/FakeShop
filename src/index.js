91b1b2f62296a884c57ce3899a2a1520d062dea9
Vladislav <vladfyklev@gmail.com>
2022-05-26 19:06:01 +0300

    добавил реакт фрагмент

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-    return <h1>Hello World</h1>
+    return (
+    <>
+        <h1>hello world</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </>)
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
