282873f1c0140756d27ef93a7be3ed739055dd86
Vladislav <vladfyklev@gmail.com>
2022-05-26 18:59:29 +0300

    Добавил компонент

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const h1 = <h1>Hello world</h1>
+function App() {
+    return <h1>Hello World</h1>
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-root.render(h1)
+root.render(<App/>)
