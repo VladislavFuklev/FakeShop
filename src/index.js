cd1ecff2b4a40ba7f81bc1d19d75b81680f6810f
Vladislav <vladfyklev@gmail.com>
2022-05-26 19:14:31 +0300

    добавил несколько компонентов

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-    return (
+  return (
     <>
-        <h1>hello world</h1>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    </>)
+      <Header />
+      <AppList />
+    </>
+  );
+}
+function Header() {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+}
+function AppList() {
+  return (
+    <>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </>
+  );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App/>)
+root.render(<App />);
