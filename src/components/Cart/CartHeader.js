f09aa5cd6175a0836e7e10659bfe92c9c1c8f6b5
Vladislav <vladfyklev@gmail.com>
2022-05-31 19:04:47 +0300

    зада отступ способом через css

---
+++ src/components/Cart/CartHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CartHeader.css"
+
+
+
+export const CartHeader = () => {
+    return (
+        <div className='cartHeader'>
+            <div>0</div>
+            <div>0$</div>
+        </div>
+    )
+}
