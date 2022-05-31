4f529e5a9c02023eb4c7bcbb2b1daede8e433986
Vladislav <vladfyklev@gmail.com>
2022-05-31 19:20:41 +0300

    сделал css модули

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./CartHeader.css"
-
+import styles from './CartHeader.module.css';
 
 
 export const CartHeader = () => {
     return (
-        <div className='cartHeader'>
+        <div className={styles.cartHeader}>
             <div>0</div>
             <div>0$</div>
         </div>
