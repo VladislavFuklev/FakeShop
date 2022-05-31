4b1e9cd5646c21569113ae1d20680517baf0d21c
Vladislav <vladfyklev@gmail.com>
2022-05-31 18:33:55 +0300

    поделючили библиотеку материал

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Header } from 'container/Header/Header'
 import { Main } from 'container/Main/Main'
 import { Footer } from 'container/Footer/Footer'
+import { CssBaseline } from '@mui/material'
 
 export const App = () => {
     return (
         <>
-            <Header/>
-            <Main/>
-            <Footer/>
+            <CssBaseline />
+            <Header />
+            <Main />
+            <Footer />
         </>
     )
 }
-
