5ac2caebfe3d8a5b59e3827b753e8cb00b2faea1
Vladislav <vladfyklev@gmail.com>
2022-06-02 19:14:17 +0300

    динамисески добавил данные на страницу с помощ map

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,6 +1,7 @@
 import { Grid, Typography } from '@mui/material'
 import React from 'react'
 import { ProductsListItem } from './ProductsListItem'
+import productsArray from 'utils/productsArray'
 
 export const ProductsList = () => {
     return (
@@ -22,33 +23,17 @@ export const ProductsList = () => {
                 justifyContent="center"
                 alignItems="center"
             >
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem
-                        name="iPhone XS"
-                        description="This is iPhone XS"
-                        type="phone"
-                        capacity="64"
-                        price="500"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem
-                        name="iPhone 12"
-                        description="This is iPhone 12"
-                        type="phone"
-                        capacity="64"
-                        price="1000"
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem
-                        name="iPhone 13"
-                        description="This is iPhone 13"
-                        type="phone"
-                        capacity="64"
-                        price="1500"
-                    />
-                </Grid>
+                {productsArray.map((product) => (
+                    <Grid item xs={12} sm={6} md={4}>
+                        <ProductsListItem
+                            name={product.name}
+                            description={product.description}
+                            type={product.type}
+                            capacity={product.capacity}
+                            price={product.price}
+                        />
+                    </Grid>
+                ))}
             </Grid>
         </>
     )
---
+++ src/utils/productsArray.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        pricename: 'iPhone XS',
+        description: 'This is iPhone XS',
+        type: 'phone',
+        capacity: '64',
+        price: '500',
+    },
+    {
+        name: 'iPhone 12',
+        description: 'This is iPhone 12',
+        type: 'phone',
+        capacity: '64',
+        price: '1000',
+    },
+    {
+        name: 'iPhone 13',
+        description: 'This is iPhone 13',
+        type: 'phone',
+        capacity: '64',
+        price: '1300',
+    },
+    {
+        name: 'iPhone 11',
+        description: 'This is iPhone 11',
+        type: 'phone',
+        capacity: '32',
+        price: '700',
+    },
+    {
+        name: 'iPhone XR',
+        description: 'This is iPhone XR',
+        type: 'phone',
+        capacity: '128',
+        price: '200',
+    },
+    {
+        name: 'iPhone 13 PRO MAX',
+        description: 'This is iPhone PRO MAX',
+        type: 'phone',
+        capacity: '512',
+        price: '2000',
+    },
+]
+
+export default products
