d557315238997fab6d5b571429de86a2c1013b72
Vladislav <vladfyklev@gmail.com>
2022-06-16 19:18:13 +0300

    добавил NavLink к хедеру и переход к корзине

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,13 +1,61 @@
 import React from 'react'
 import Button from '@mui/material/Button'
+import { Link, NavLink } from 'react-router-dom'
+import './menu.scss'
 
 export const Menu = () => {
     return (
         <>
-            <Button color="inherit">HOME</Button>
-            <Button color="inherit">PRODUCTS</Button>
-            <Button color="inherit">SHIPPING</Button>
-            <Button color="inherit">PAYMENT</Button>
+           <Button color="inherit">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                        isActive ? 'active-menu-NavLink' : 'menu-NavLink'
+                    }
+                >
+                    Home
+                </NavLink>
+            </Button>
+            <Button color="inherit">
+                <NavLink
+                    to="/products"
+                    className={({ isActive }) =>
+                        isActive ? 'active-menu-NavLink' : 'menu-NavLink'
+                    }
+                >
+                    PRODUCTS
+                </NavLink>
+            </Button>
+            <Button color="inherit">
+                <NavLink
+                    to="/payment"
+                    className={({ isActive }) =>
+                        isActive ? 'active-menu-NavLink' : 'menu-NavLink'
+                    }
+                >
+                    PAYMENT
+                </NavLink>
+            </Button>
+            <Button color="inherit">
+                <NavLink
+                    to="/shipping"
+                    className={({ isActive }) =>
+                        isActive ? 'active-menu-NavLink' : 'menu-NavLink'
+                    }
+                >
+                    SHIPPING
+                </NavLink>
+            </Button>
+            <Button color="inherit">
+                <NavLink
+                    to="/cart"
+                    className={({ isActive }) =>
+                        isActive ? 'active-menu-NavLink' : 'menu-NavLink'
+                    }
+                >
+                    CART
+                </NavLink>
+            </Button>
         </>
     )
 }
