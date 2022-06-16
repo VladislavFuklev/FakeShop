4122069b82157371463f6cbeaf386dd7b5a7285e
Vladislav <vladfyklev@gmail.com>
2022-06-16 18:19:55 +0300

    реализовал подсчет суммы товаров

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import './CartHeader.scss'
-import {keys} from 'lodash'
+import { keys } from 'lodash'
+import products from 'utils/productsArray'
 
-export const CartHeader = ({productsInCart}) => {
+
+
+export const CartHeader = ({ productsInCart}) => {
     return (
         <div className="cartHeader">
-           {keys(productsInCart).map((productId) => (
-            <div key={productId}>
-                {productId}: {productsInCart[productId]}
+            <div>
+                {keys(productsInCart).map((productId) => (
+                    <div key={productId}>
+                        {productId}: {productsInCart[productId]}
+                    </div>
+                ))}
             </div>
-           ))}
+            <div>total:
+                {Object.keys(productsInCart).reduce((previousValue, currentItem) => (
+                   previousValue + (productsInCart[currentItem]) * products[currentItem - 1].price
+                ), 0)}
+            </div>
         </div>
     )
 }
