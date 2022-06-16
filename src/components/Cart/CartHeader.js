2d6ee78c88e90acb6ceb8622b602926c6dfe0a16
Vladislav <vladfyklev@gmail.com>
2022-06-16 18:41:41 +0300

    const productsObject = products.reduce( сделали

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -3,22 +3,35 @@ import './CartHeader.scss'
 import { keys } from 'lodash'
 import products from 'utils/productsArray'
 
+const productsObject = products.reduce(
+    (obj, product) => ({
+        ...obj,
+        [product.id]: product,
+    }),
+    {}
+)
 
-
-export const CartHeader = ({ productsInCart}) => {
+console.log(productsObject)
+export const CartHeader = ({ productsInCart }) => {
     return (
         <div className="cartHeader">
             <div>
                 {keys(productsInCart).map((productId) => (
                     <div key={productId}>
-                        {productId}: {productsInCart[productId]}
+                        {productsObject[productId].name}:{' '}
+                        {productsInCart[productId]}
                     </div>
                 ))}
             </div>
-            <div>total:
-                {Object.keys(productsInCart).reduce((previousValue, currentItem) => (
-                   previousValue + (productsInCart[currentItem]) * products[currentItem - 1].price
-                ), 0)}
+            <div>
+                total:
+                {Object.keys(productsInCart).reduce(
+                    (previousValue, currentItem) =>
+                        previousValue +
+                        productsInCart[currentItem] *
+                        productsObject[currentItem].price,
+                    0
+                )}
             </div>
         </div>
     )
