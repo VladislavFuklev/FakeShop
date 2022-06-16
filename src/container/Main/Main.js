7dbcf9ace6e2f61a76dcf8762657310160cfbf6b
Vladislav <vladfyklev@gmail.com>
2022-06-16 19:25:02 +0300

    реализовал переход на корзину

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,5 +1,6 @@
 import { Container } from '@mui/material'
 import { ProductsList } from 'components/Products/ProductsList'
+import { CartPage } from 'pages/Cart/CartPage'
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
@@ -9,6 +10,7 @@ export const Main = ({ click }) => {
             <Container>
                 <Routes>
                     <Route path="/" element={<ProductsList click={click} />} />
+                    <Route path="/cart" element ={<CartPage/>}/>
                 </Routes>
             </Container>
         </>
