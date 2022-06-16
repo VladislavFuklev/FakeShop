e0b5986a6200e942eaf839f0d567d255e17f8d75
Vladislav <vladfyklev@gmail.com>
2022-06-16 19:29:47 +0300

    добавил маршруты на все страницы меню

--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -1,6 +1,9 @@
 import { Container } from '@mui/material'
 import { ProductsList } from 'components/Products/ProductsList'
 import { CartPage } from 'pages/Cart/CartPage'
+import { Payment } from 'pages/Payment/PaymentPage'
+import { ProductPage } from 'pages/Products/ProductsPage'
+import { ShippingPage } from 'pages/Shipping/ShippingPage'
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
@@ -11,6 +14,9 @@ export const Main = ({ click }) => {
                 <Routes>
                     <Route path="/" element={<ProductsList click={click} />} />
                     <Route path="/cart" element ={<CartPage/>}/>
+                    <Route path="/payment" element ={<Payment/>}/>
+                    <Route path="/products" element ={<ProductPage/>}/>
+                    <Route path="/shipping" element ={<ShippingPage/>}/>
                 </Routes>
             </Container>
         </>
