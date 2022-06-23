84747ed26cd627ceaacbb839d3475ac3a42b6508
Vladislav <vladfyklev@gmail.com>
2022-06-23 20:42:33 +0300

    добавил отправку отзыва и отображение его на стр

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,10 +1,38 @@
-import { Card, TextField } from '@mui/material'
+import { Button, Card, TextField } from '@mui/material'
 import React, { useState } from 'react'
 import arrReviews from './ArrRewies'
 import './reviews.scss'
 
 export const Reviews = () => {
     const [reviews, SetReviews] = useState(arrReviews)
+
+    const [newReview, SetNewReviews] = useState({
+        name: '',
+        text: '',
+    })
+
+    const handleName = (e) =>
+        SetNewReviews((prevState) => ({
+            ...prevState,
+            name: e.target.value,
+        }))
+
+    const handleText = (e) =>
+        SetNewReviews((prevState) => ({
+            ...prevState,
+            text: e.target.value,
+        }))
+
+    const reviewsSend = (e) => {
+        e.preventDefault()
+        SetNewReviews({
+            name: '',
+            text: '',
+        })
+        SetReviews((value) => {
+            return [...value, newReview]
+        })
+    }
 
     return (
         <>
@@ -18,11 +46,26 @@ export const Reviews = () => {
             <h3>Please leave review about this product</h3>
             <form>
                 <div>
-                    <TextField size='small' label='Name' margin='normal'/>
+                    <TextField
+                        size="small"
+                        label="Name"
+                        margin="normal"
+                        value={newReview.name}
+                        onChange={handleName}
+                    />
                 </div>
                 <div>
-                    <TextField size='small' label='Text' margin='normal'/>
+                    <TextField
+                        size="small"
+                        label="Text"
+                        margin="normal"
+                        value={newReview.text}
+                        onChange={handleText}
+                    />
                 </div>
+                <Button variant="contained" onClick={reviewsSend}>
+                    Leave review
+                </Button>
             </form>
         </>
     )
